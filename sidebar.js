d3495e4d85b852c74a38307c8121115d307dad3a
chaneque13 <jvega19@ucol.mx>
2023-04-13 01:52:25 -0600

    Nuevas cositas

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.querySelector('.icono');
+    const sidebar = document.querySelector('.sidebar');
+
+    toggleButton.addEventListener('click', function() {
+        sidebar.classList.toggle('active'); // Cambiar 'sidebar' por 'active'
+    });
+});
